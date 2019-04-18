095448dc3920b7a94597b62f385605de7bc5dffe
Nasser Jeary <36063816+jeary1988@users.noreply.github.com>
2019-04-18 09:58:21 -0700

    done
    done

---
+++ cartBasket.js
@@ -0,0 +1,64 @@
+ var PizzaOrder = {
+      cname: $('#cName').val(),
+      uid: userID,
+      cEmail: $('#cEmail').val(),
+      cPhone: $('#cPhone').val(),
+      cOrder: $('#orderDetails').val(),
+      orderStatus: '1'
+  };
+    
+
+
+
+function AddPizzaToLcalStorage(PizzaOrder){
+    
+ 
+ 
+    var check =  localStorage.getItem('orderArray');
+    // check if the localSotage has any DB been created with a key 'orderArray'
+      if(check === null){
+         
+          var orderList = [];
+          
+      orderList.push(pizzaObject);
+    var x = JSON.stringify(orderList);
+    localStorage.setItem('orderArray', x); 
+        
+      }else{/*-----------------------------------------*/  
+       /* check if the array in the localStorage has 'orderArray' key already if so then bring it */
+      var order = JSON.parse(localStorage.getItem('orderArray'));
+   
+      //adding a new order to the list
+      order.push(pizzaObject);
+
+      // add it as JSON object
+       var x = JSON.stringify(order);
+    localStorage.setItem('orderArray', x); 
+        
+  }
+    
+}
+ 
+
+
+
+
+
+function getPizzafromLS(){
+    /*-----------------------------------------*/
+      var order = JSON.parse(localStorage.getItem('orderArray'));
+    console.log('array after parsing json:' + order);
+          $('#localS').val(' '); 
+     $.each(order, function(index, item){
+      // loop through - for instance    
+   var personName = item.name;  
+  
+var toTextArea = `<p>${pName}</p>`;  //backticks
+     
+$('#localS').append(toTextArea);
+        
+     });  /*----end $.each() ---*/
+/*-----------------------------------------*/                  
+}
+
+getPizzafromLS()
